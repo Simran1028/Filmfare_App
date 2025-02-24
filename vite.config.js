fe805ec13d31c086a24ce274854db286b483be44
Simran1028 <simrandeepkaur06488@gmail.com>
2025-02-24 12:45:34 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  alias: {
-    "@components": path.resolve(__dirname, "src/components"),
-    "@apis": path.resolve(__dirname, "src/common/apis"),
-  },
 });
