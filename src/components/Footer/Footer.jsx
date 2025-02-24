13d71496d0574a674035babcffdb582e84bf939f
Simran1028 <simrandeepkaur06488@gmail.com>
2025-02-24 12:54:32 +0530

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Footer.scss';
+import React from "react";
+import "./Footer.scss";
 
 const footer = () => {
-  return (<>
-  <div className="footer">
-    <div>Filmfare App</div>
-    <div>�� 2021 Filmfare. All rights reserved.</div>
-  </div>
-  </>);
-}
+  return (
+    <>
+      <div className="footer">
+        <div>Filmfare App</div>
+        <div>�� 2021 Filmfare. All rights reserved.</div>
+      </div>
+    </>
+  );
+};
 
-export default footer
+export default footer;
