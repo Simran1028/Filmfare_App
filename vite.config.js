9a10107df830b131f2dc877778ff50334d8638e1
Simran1028 <simrandeepkaur06488@gmail.com>
2025-02-24 12:41:47 +0530

    changes in config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  alias: {
+    "@components": path.resolve(__dirname, "src/components"),
+    "@apis": path.resolve(__dirname, "src/common/apis"),
+  },
+});
