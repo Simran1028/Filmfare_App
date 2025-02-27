af78a79438b7457476e92c6635d71564aa8df436
Simran1028 <simrandeepkaur06488@gmail.com>
2025-02-27 23:42:25 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Footer from "../src/components/Footer/footer";
+import Footer from "./components/Footer/footer";
 import Home from "./../src/components/Home/home";
 import MovieDetails from "./../src/components/MovieDetails/MovieDetails";
 import PageNotFound from "./../src/components/Page_Not_Found/PageNotFound";
 import Header from "./../src/components/Header/header";
+// import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
