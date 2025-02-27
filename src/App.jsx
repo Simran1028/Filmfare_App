4f16ba05dd192d5926f48ebcf15f3070963cca16
Simran1028 <simrandeepkaur06488@gmail.com>
2025-02-27 14:53:25 +0530

    saved changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Footer from "./components/Footer/footer";
+// import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
 import MovieDetails from "./components/MovieDetails/movieDetails";
 import PageNotFound from "./components/Page_Not_Found/pageNotFound";
@@ -19,7 +19,7 @@ function App() {
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </div>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </div>
   );
